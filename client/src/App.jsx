275e508381cebf895f93b7a02b43295e8e631413
vanya-vb <vbozadzhieva29@gmail.com>
2025-08-06 15:04:34 +0300

    add register page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import Header from './components/Header/Header'
 import HeroSection from './components/HeroSection/HeroSection'
 import LoginPage from './components/LoginPage/LoginPage'
+import RegisterPage from './components/RegisterPage/RegisterPage'
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Header />
 			<HeroSection />
 			<LoginPage />
+			<RegisterPage/>
 		</>
 	)
 }
