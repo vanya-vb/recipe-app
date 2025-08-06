a51cd750207984a55b5d8171b3e7d81576776ba0
vanya-vb <vbozadzhieva29@gmail.com>
2025-08-06 14:56:17 +0300

    add login page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 
 import Header from './components/Header/Header'
 import HeroSection from './components/HeroSection/HeroSection'
+import LoginPage from './components/LoginPage/LoginPage'
 
 function App() {
-
-
 	return (
 		<>
 			<Header />
 			<HeroSection />
+			<LoginPage />
 		</>
 	)
 }
