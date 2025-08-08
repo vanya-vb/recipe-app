cf6eb248d6d4a5c6dfe545b68308cb3bf5be56df
vanya-vb <vbozadzhieva29@gmail.com>
2025-08-08 14:26:54 +0300

    move recipe data to external file

--- client/src/components/RecipesPage/RecipesPage.jsx
+++ client/src/components/RecipesPage/RecipesPage.jsx
@@ -2,51 +2,6 @@ import { useEffect, useState } from "react";
 import recipeService from "../../services/recipeService";
 import RecipeItem from "./RecipeItem/RecipeItem";
 
-const menu = [
-    {
-        id: 1,
-        title: "buttermilk pancakes",
-        category: "breakfast",
-        img: "/src/components/RecipesPage/images/item-1.jpeg",
-        instructions: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
-    },
-    {
-        id: 2,
-        title: "diner double",
-        category: "lunch",
-        img: "/src/components/RecipesPage/images/item-2.jpeg",
-        instructions: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-    },
-    {
-        id: 3,
-        title: "godzilla milkshake",
-        category: "shakes",
-        img: "/src/components/RecipesPage/images/item-3.jpeg",
-        instructions: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
-    },
-    {
-        id: 4,
-        title: "country delight",
-        category: "breakfast",
-        img: "/src/components/RecipesPage/images/item-4.jpeg",
-        instructions: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
-    },
-    {
-        id: 5,
-        title: "egg attack",
-        category: "lunch",
-        img: "/src/components/RecipesPage/images/item-5.jpeg",
-        instructions: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
-    },
-    {
-        id: 6,
-        title: "oreo dream",
-        category: "shakes",
-        price: 18.99,
-        img: "/src/components/RecipesPage/images/item-6.jpeg",
-        instructions: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
-    },
-];
 
 export default function RecipesPage() {
     const [recipes, setRecipes] = useState([]);
