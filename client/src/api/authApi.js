fdd7e29939e4f41f8a63a47b6fa9d3a39385e0fc
vanya-vb <vbozadzhieva29@gmail.com>
2025-08-14 13:12:01 +0300

    add abort controller to useLogin

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,13 +1,26 @@
+import { useEffect, useRef } from "react";
 import request from "../utils/request";
 
 const baseUrl = 'http://localhost:3030/users';
 
 export const useLogin = () => {
+    const abortRef = useRef(new AbortController());
+
     const login = async (email, password) => {
-        const result = await request.post(`${baseUrl}/login`, { email, password });
+        const result = await request.post(
+            `${baseUrl}/login`,
+            { email, password },
+            { signal: abortRef.current.signal },
+        );
 
         return result;
     }
+
+    useEffect(() => {
+        const abortController = abortRef.current;
+
+        return () => abortController.abort();
+    }, []);
 
     return {
         login,
