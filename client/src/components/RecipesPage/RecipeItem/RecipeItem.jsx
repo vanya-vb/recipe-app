0f849ae0e80f65aa82f5443ae327b00283dc44b9
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-25 15:07:07 +0300

    fix link to recipe

--- client/src/components/RecipesPage/RecipeItem/RecipeItem.jsx
+++ client/src/components/RecipesPage/RecipeItem/RecipeItem.jsx
@@ -5,19 +5,18 @@ export default function RecipeItem({ _id, title, category, imageUrl, ingredients
 
     return (
         <article className="grid gap-4 sm:grid-cols-[200px_1fr] capitalize">
-            <div className="relative">
-                <img
-                    src={imageUrl}
-                    alt={title}
-                    className="h-48 w-full object-cover border-4 border-olivine rounded-md md:h-40"
-                />
-                <Link
-                    to={`/recipes/${_id}/details`}
-                    className="absolute bottom-2 right-2 bg-olivine text-night px-2 py-1 rounded-md text-xs font-semibold transition hover:bg-olivine/80"
-                >
-                    Details
-                </Link>
-            </div>
+            <Link to={`/recipes/${_id}/details`}>
+                <div className="relative">
+                    <img
+                        src={imageUrl}
+                        alt={title}
+                        className="h-48 w-full object-cover border-4 border-olivine rounded-md md:h-40"
+                    />
+                    <button className="absolute bottom-2 right-2 bg-olivine text-night px-2 py-1 rounded-md text-xs font-semibold transition hover:bg-olivine/80 cursor-pointer">
+                        Details
+                    </button>
+                </div>
+            </Link>
 
             <div>
                 <header className="flex justify-between items-center border-b border-platinum pb-1 mb-2">
@@ -26,7 +25,7 @@ export default function RecipeItem({ _id, title, category, imageUrl, ingredients
                         {category}
                     </span>
                 </header>
-                <p className="text-platinum/60 text-xs">{ingredients}</p>
+                <p className="text-platinum/80 text-xs">{ingredients}</p>
             </div>
         </article>
 
