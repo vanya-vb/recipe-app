a80e3005061f806f0ce4ffe6a18a442a6e14e76d
vanya-vb <vbozadzhieva29@gmail.com>
2025-10-14 16:25:53 +0300

    fix keys security

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKZ-8J5zurdyZ4QvwmCqfiPDEkxcHmnI8",
-  authDomain: "recipe-app-470807.firebaseapp.com",
-  projectId: "recipe-app-470807",
-  storageBucket: "recipe-app-470807.firebasestorage.app",
-  messagingSenderId: "836625707452",
-  appId: "1:836625707452:web:d10804d73baf3625273bb9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
