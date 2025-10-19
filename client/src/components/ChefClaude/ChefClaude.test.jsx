4d9a6411a571c55844dc9de4e98b0c33831f0a3b
vanya-vb <vbozadzhieva29@gmail.com>
2025-10-19 16:31:42 +0300

    add an integration test

--- client/src/components/ChefClaude/ChefClaude.test.jsx
+++ client/src/components/ChefClaude/ChefClaude.test.jsx
@@ -1,7 +1,24 @@
-import { test, expect, describe } from 'vitest';
+vi.mock('../../api/ai', () => ({
+    getRecipeFromChefClaude: vi.fn().mockResolvedValue('Mocked recipe content'),
+}));
+
+import { test, expect, describe, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import ChefClaude from './ChefClaude';
+import { getRecipeFromChefClaude } from '../../api/ai'
+
+const ingredients = ['pasta', 'chicken', 'oregano', 'cream'];
+
+async function addIngredients(user, ingredients) {
+    const input = screen.getByRole('textbox');
+    const addBtn = screen.getByRole('button', { name: 'Add ingredient' });
+
+    for (let ingredient of ingredients) {
+        await user.type(input, ingredient)
+        await user.click(addBtn);
+    }
+};
 
 describe('ChefClaude', () => {
     test('renders the input values in the list', async () => {
@@ -32,20 +49,24 @@ describe('ChefClaude', () => {
         // arrange
         const user = userEvent.setup();
         render(<ChefClaude />);
-        const input = screen.getByRole('textbox');
-        const addBtn = screen.getByRole('button', { name: 'Add ingredient' });
         const getRecipeBtn = screen.getByRole('button', { name: 'Get a recipe' });
 
         // act 
-        const ingredients = ['pasta', 'chicken', 'oregano', 'cream'];
-
-        for (let ingredient of ingredients) {
-            await user.type(input, ingredient)
-            await user.click(addBtn);
-        }
+        await addIngredients(user, ingredients);
 
         // assert
         expect(screen.getAllByRole('listitem')).toHaveLength(4);
         expect(getRecipeBtn).toBeEnabled();
     });
-})
+
+    test('integration test: calls getRecipeFromChefClaude when button clicked', async () => {
+        const user = userEvent.setup();
+        render(<ChefClaude />);
+        const getRecipeBtn = screen.getByRole('button', { name: 'Get a recipe' });
+
+        await addIngredients(user, ingredients);
+        await user.click(getRecipeBtn);
+
+        expect(getRecipeFromChefClaude).toHaveBeenCalledWith(ingredients);
+    });
+});
